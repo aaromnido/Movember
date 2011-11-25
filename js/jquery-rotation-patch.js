652884d9610494798af71ca7ca3f9eac40a23a83
aaromnido <aaromnido@gmail.com>
2011-11-25 10:19:19 +0100

    First template version

---
+++ js/jquery-rotation-patch.js
@@ -0,0 +1,410 @@
+(function ($) {
+    // Monkey patch jQuery 1.3.1+ css() method to support CSS 'transform'
+    // property uniformly across Webkit/Safari/Chrome, Firefox 3.5+, and IE 9+.
+    // 2009-2011 Zachary Johnson www.zachstronaut.com
+    // Updated 2011.05.04 (May the fourth be with you!)
+    function getTransformProperty(element)
+    {
+        // Try transform first for forward compatibility
+        // In some versions of IE9, it is critical for msTransform to be in
+        // this list before MozTranform.
+        var properties = ['transform', 'WebkitTransform', 'msTransform', 'MozTransform', 'OTransform'];
+        var p;
+        while (p = properties.shift())
+        {
+            if (typeof element.style[p] != 'undefined')
+            {
+                return p;
+            }
+        }
+        
+        // Default to transform also
+        return 'transform';
+    }
+    
+    var _propsObj = null;
+    
+    var proxied = $.fn.css;
+    $.fn.css = function (arg, val)
+    {
+        // Temporary solution for current 1.6.x incompatibility, while
+        // preserving 1.3.x compatibility, until I can rewrite using CSS Hooks
+        if (_propsObj === null)
+        {
+            if (typeof $.cssProps != 'undefined')
+            {
+                _propsObj = $.cssProps;
+            }
+            else if (typeof $.props != 'undefined')
+            {
+                _propsObj = $.props;
+            }
+            else
+            {
+                _propsObj = {}
+            }
+        }
+        
+        // Find the correct browser specific property and setup the mapping using
+        // $.props which is used internally by jQuery.attr() when setting CSS
+        // properties via either the css(name, value) or css(properties) method.
+        // The problem with doing this once outside of css() method is that you
+        // need a DOM node to find the right CSS property, and there is some risk
+        // that somebody would call the css() method before body has loaded or any
+        // DOM-is-ready events have fired.
+        if
+        (
+            typeof _propsObj['transform'] == 'undefined'
+            &&
+            (
+                arg == 'transform'
+                ||
+                (
+                    typeof arg == 'object'
+                    && typeof arg['transform'] != 'undefined'
+                )
+            )
+        )
+        {
+            _propsObj['transform'] = getTransformProperty(this.get(0));
+        }
+        
+        // We force the property mapping here because jQuery.attr() does
+        // property mapping with jQuery.props when setting a CSS property,
+        // but curCSS() does *not* do property mapping when *getting* a
+        // CSS property.  (It probably should since it manually does it
+        // for 'float' now anyway... but that'd require more testing.)
+        //
+        // But, only do the forced mapping if the correct CSS property
+        // is not 'transform' and is something else.
+        if (_propsObj['transform'] != 'transform')
+        {
+            // Call in form of css('transform' ...)
+            if (arg == 'transform')
+            {
+                arg = _propsObj['transform'];
+                
+                // User wants to GET the transform CSS, and in jQuery 1.4.3
+                // calls to css() for transforms return a matrix rather than
+                // the actual string specified by the user... avoid that
+                // behavior and return the string by calling jQuery.style()
+                // directly
+                if (typeof val == 'undefined' && jQuery.style)
+                {
+                    return jQuery.style(this.get(0), arg);
+                }
+            }
+
+            // Call in form of css({'transform': ...})
+            else if
+            (
+                typeof arg == 'object'
+                && typeof arg['transform'] != 'undefined'
+            )
+            {
+                arg[_propsObj['transform']] = arg['transform'];
+                delete arg['transform'];
+            }
+        }
+        
+        return proxied.apply(this, arguments);
+    };
+})(jQuery);
+
+
+
+(function ($) {
+    // Monkey patch jQuery 1.3.1+ to add support for setting or animating CSS
+    // scale and rotation independently.
+    // 2009-2010 Zachary Johnson www.zachstronaut.com
+    // Updated 2010.11.06
+    var rotateUnits = 'deg';
+    
+    $.fn.rotate = function (val)
+    {
+        var style = $(this).css('transform') || 'none';
+        
+        if (typeof val == 'undefined')
+        {
+            if (style)
+            {
+                var m = style.match(/rotate\(([^)]+)\)/);
+                if (m && m[1])
+                {
+                    return m[1];
+                }
+            }
+            
+            return 0;
+        }
+        
+        var m = val.toString().match(/^(-?\d+(\.\d+)?)(.+)?$/);
+        if (m)
+        {
+            if (m[3])
+            {
+                rotateUnits = m[3];
+            }
+            
+            $(this).css(
+                'transform',
+                style.replace(/none|rotate\([^)]*\)/, '') + 'rotate(' + m[1] + rotateUnits + ')'
+            );
+        }
+        
+        return this;
+    }
+    
+    // Note that scale is unitless.
+    $.fn.scale = function (val, duration, options)
+    {
+        var style = $(this).css('transform');
+        
+        if (typeof val == 'undefined')
+        {
+            if (style)
+            {
+                var m = style.match(/scale\(([^)]+)\)/);
+                if (m && m[1])
+                {
+                    return m[1];
+                }
+            }
+            
+            return 1;
+        }
+        
+        $(this).css(
+            'transform',
+            style.replace(/none|scale\([^)]*\)/, '') + 'scale(' + val + ')'
+        );
+        
+        return this;
+    }
+
+    // fx.cur() must be monkey patched because otherwise it would always
+    // return 0 for current rotate and scale values
+    var curProxied = $.fx.prototype.cur;
+    $.fx.prototype.cur = function ()
+    {
+        if (this.prop == 'rotate')
+        {
+            return parseFloat($(this.elem).rotate());
+        }
+        else if (this.prop == 'scale')
+        {
+            return parseFloat($(this.elem).scale());
+        }
+        
+        return curProxied.apply(this, arguments);
+    }
+    
+    $.fx.step.rotate = function (fx)
+    {
+        $(fx.elem).rotate(fx.now + rotateUnits);
+    }
+    
+    $.fx.step.scale = function (fx)
+    {
+        $(fx.elem).scale(fx.now);
+    }
+    
+    /*
+    
+    Starting on line 3905 of jquery-1.3.2.js we have this code:
+    
+    // We need to compute starting value
+    if ( unit != "px" ) {
+        self.style[ name ] = (end || 1) + unit;
+        start = ((end || 1) / e.cur(true)) * start;
+        self.style[ name ] = start + unit;
+    }
+    
+    This creates a problem where we cannot give units to our custom animation
+    because if we do then this code will execute and because self.style[name]
+    does not exist where name is our custom animation's name then e.cur(true)
+    will likely return zero and create a divide by zero bug which will set
+    start to NaN.
+    
+    The following monkey patch for animate() gets around this by storing the
+    units used in the rotation definition and then stripping the units off.
+    
+    */
+    
+    var animateProxied = $.fn.animate;
+    $.fn.animate = function (prop)
+    {
+        if (typeof prop['rotate'] != 'undefined')
+        {
+            var m = prop['rotate'].toString().match(/^(([+-]=)?(-?\d+(\.\d+)?))(.+)?$/);
+            if (m && m[5])
+            {
+                rotateUnits = m[5];
+            }
+            
+            prop['rotate'] = m[1];
+        }
+        
+        return animateProxied.apply(this, arguments);
+    }
+})(jQuery);
+
+
+
+// http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+// major changes by Paul Armstrong and Zachary Johnson
+function enable_smooth_scroll() {
+    function filterPath(string) {
+        return string
+                .replace(/^\//,'')
+                .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+                .replace(/\/$/,'');
+    }
+
+    var locationPath = filterPath(location.pathname);
+
+    var scrollElement = 'html, body';
+    $('html, body').each(function () {
+        var initScrollTop = $(this).attr('scrollTop');
+        $(this).attr('scrollTop', initScrollTop + 1);
+        if ($(this).attr('scrollTop') == initScrollTop + 1) {
+            scrollElement = this.nodeName.toLowerCase();
+            $(this).attr('scrollTop', initScrollTop);
+            return false;
+        }    
+    });
+    
+    $('a[href*=#]').each(function() {
+        var thisPath = filterPath(this.pathname) || locationPath;
+        if  (   locationPath == thisPath
+                && (location.hostname == this.hostname || !this.hostname)
+                && this.hash.replace(/#/, '')
+            ) {
+                if ($(this.hash).length) {
+                    $(this).click(function(event) {
+                        var targetOffset = $(this.hash).offset().top;
+                        var target = this.hash;
+                        event.preventDefault();
+                        $(scrollElement).animate({scrollTop: targetOffset}, 500, function() {
+                            location.hash = target;
+                        });
+                    });
+                }
+        }
+    });
+}
+
+
+/**
+ * jQuery Default Value Plugin v1.0
+ * Progressive enhancement technique for inital input field values
+ *
+ * The MIT License
+ * 
+ * Copyright (c) 2007 Paul Campbell (pauljamescampbell.co.uk)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Improvements by Zachary Johnson 2009
+ */
+(function($) {
+	$.fn.defaultVal = function() {
+		// Scope
+		var elements = this;
+		var args = arguments;
+		var c = 0;
+		
+		return (
+            elements.each(function() {
+				var el = $(this);
+				var value = args[c++];
+
+                if (el.val() == '') {
+                    el.val(value);
+                }
+                
+				el.focus(function() {
+					if(el.val() == value) {
+						el.val('');
+					}
+					
+					el.blur(function() {
+    					if(el.val() == '') {
+    						el.val(value);
+    					}
+    				});
+				});
+			})
+    	);
+    };
+})(jQuery);
+
+
+/*! Copyright (c) 2010 Brandon Aaron (http://brandonaaron.net)
+ * Licensed under the MIT License (LICENSE.txt).
+ */
+(function($) {
+    // backgroundPosition[X,Y] get hooks
+    var $div = $('<div style="background-position: 3px 5px">');
+    $.support.backgroundPosition   = $div.css('backgroundPosition')  === "3px 5px" ? true : false;
+    $.support.backgroundPositionXY = $div.css('backgroundPositionX') === "3px" ? true : false;
+    $div = null;
+
+    var xy = ["X","Y"];
+
+    // helper function to parse out the X and Y values from backgroundPosition
+    function parseBgPos(bgPos) {
+        var parts  = bgPos.split(/\s/),
+            values = {
+                "X": parts[0],
+                "Y": parts[1]
+            };
+        return values;
+    }
+
+    if (!$.support.backgroundPosition && $.support.backgroundPositionXY) {
+        $.cssHooks.backgroundPosition = {
+            get: function( elem, computed, extra ) {
+                return $.map(xy, function( l, i ) {
+                    return $.css(elem, "backgroundPosition" + l);
+                }).join(" ");
+            },
+            set: function( elem, value ) {
+                $.each(xy, function( i, l ) {
+                    var values = parseBgPos(value);
+                    elem.style[ "backgroundPosition" + l ] = values[ l ];
+                });
+            }
+        };
+    }
+
+    if ($.support.backgroundPosition && !$.support.backgroundPositionXY) {
+        $.each(xy, function( i, l ) {
+            $.cssHooks[ "backgroundPosition" + l ] = {
+                get: function( elem, computed, extra ) {
+                    var values = parseBgPos( $.css(elem, "backgroundPosition") );
+                    return values[ l ];
+                },
+                set: function( elem, value ) {
+                    var values = parseBgPos( $.css(elem, "backgroundPosition") ),
+                        isX = l === "X";
+                    elem.style.backgroundPosition = (isX ? value : values[ "X" ]) + " " + 
+                                                    (isX ? values[ "Y" ] : value);
+                }
+            };
+            $.fx.step[ "backgroundPosition" + l ] = function( fx ) {
+                $.cssHooks[ "backgroundPosition" + l ].set( fx.elem, fx.now + fx.unit );
+            };
+        });
+    }
+})(jQuery);
+
+
